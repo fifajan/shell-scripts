7b11b274ea8f9eced4800339682c4018bffec8f8
Alexey Ivchenko <me@swdevelop.net>
2017-03-13 10:22:16 +0200

    Fixed typo, fixed IP.

--- nas_backup_pusher.sh
+++ nas_backup_pusher.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 # this script requires lftp package, NAS (network attached storage)
-# accessable by FTP protocol (user shoul have write perms)
+# accessable by FTP protocol (user should have write perms)
 
-NAS_IP="181.191.101.111" # domain/host name should work too 
+NAS_IP="128.64.32.16" # domain/host name should work too 
 NAS_AUTH="user_name:user_pwd"
 FTP_N_OPTS="lftp -e"
 
